b195c77c6bea719adbf764d32958ff6f2640c55b
bw007 <mr.abdulaziz00791@gmail.com>
2024-06-14 11:29:04 +0500

    Gulp setup complete

--- gulpfile.js
+++ gulpfile.js
@@ -1,173 +1,110 @@
-const gulp = require("gulp");
+import gulp from 'gulp';
+import browsersync from 'browser-sync';
+import gulpclean from 'gulp-clean';
+import autoprefixer from 'gulp-autoprefixer';
+import cssbeautify from 'gulp-cssbeautify';
+import cssnano from 'gulp-cssnano';
+import imagemin from 'gulp-imagemin';
+import plumber from 'gulp-plumber';
+import gulppug from 'gulp-pug';
+import rename from 'gulp-rename';
+import rigger from 'gulp-rigger';
+import gulpSass from 'gulp-sass';
+import * as sass from 'sass';
+import uglify from 'gulp-uglify';
 
-const { src, dest } = require("gulp");
+const sassCompiler = gulpSass(sass);
 
-const browsersync = require("browser-sync");
-
-const gulpclean = require("gulp-clean");
-
-const autoprefixer = require("gulp-autoprefixer");
-
-const cssbeautify = require("gulp-cssbeautify");
-
-const cssnano = require("gulp-cssnano");
-
-const imagemin = require("gulp-imagemin");
-
-const plumber = require("gulp-plumber");
-
-const gulppug = require("gulp-pug");
-
-const rename = require("gulp-rename");
-
-const rigger = require("gulp-rigger");
-
-const sass = require("gulp-sass")(require("node-sass"));
-
-const uglify = require("gulp-uglify");
-
-let path = {
+const path = {
   build: {
-    html: "dist/",
-
-    js: "dist/assets/js/",
-
-    css: "dist/assets/css/",
-
-    font: "dist/assets/fonts/",
-
-    images: "dist/assets/img/",
+    html: 'dist/',
+    js: 'dist/js/',
+    css: 'dist/css/',
+    font: 'dist/fonts/',
+    images: 'dist/imgs/',
   },
-
   src: {
-    font: "src/assets/fonts/*",
-
-    pug: "src/pug/pages/**/*.pug",
-
-    js: "src/assets/js/*.js",
-
-    css: "src/assets/sass/style.scss",
-
-    images: "src/assets/img/**/*.{jpg,png,svg,gif,ico}",
+    font: 'src/fonts/*',
+    pug: 'src/pug/pages/**/*.pug',
+    js: 'src/js/*.js',
+    css: 'src/scss/main.scss',
+    images: 'src/imgs/**/*.{jpg,png,svg,gif,ico}',
   },
-
   watch: {
-    font: "src/assets/fonts/*",
-
-    pug: "src/pug/**/*.pug",
-
-    js: "src/assets/js/**/*.js",
-
-    css: "src/assets/sass/**/*.scss",
-
-    images: "src/assets/img/**/*.{jpg,png,svg,gif,ico}",
+    font: 'src/fonts/*',
+    pug: 'src/pug/**/*.pug',
+    js: 'src/js/**/*.js',
+    css: 'src/scss/**/*.scss',
+    images: 'src/imgs/**/*.{jpg,png,svg,gif,ico}',
   },
-
-  clean: "./dist",
+  clean: './dist',
 };
 
 function browserSync(done) {
   browsersync.init({
     server: {
-      baseDir: "./dist/",
+      baseDir: './dist/',
     },
-
     port: 3000,
   });
+  done();
 }
 
-async function css() {
-  return src(path.src.css, { base: "src/assets/sass/" })
+function css() {
+  return gulp.src(path.src.css, { base: 'src/scss/' })
     .pipe(plumber())
-
-    .pipe(sass())
-
+    .pipe(sassCompiler())
     .pipe(autoprefixer())
-
     .pipe(cssbeautify())
-
-    .pipe(dest(path.build.css))
-
+    .pipe(gulp.dest(path.build.css))
     .pipe(cssnano())
-
-    .pipe(rename({ suffix: ".min", extname: ".css" }))
-
-    .pipe(dest(path.build.css))
-
+    .pipe(rename({ suffix: '.min', extname: '.css' }))
+    .pipe(gulp.dest(path.build.css))
     .pipe(browsersync.stream());
 }
 
-async function js() {
-  return src(path.src.js, { base: "./src/assets/js/" })
+function js() {
+  return gulp.src(path.src.js, { base: './src/js/' })
     .pipe(plumber())
-
     .pipe(rigger())
-
-    .pipe(dest(path.build.js))
-
+    .pipe(gulp.dest(path.build.js))
     .pipe(uglify())
-
-    .pipe(rename({ suffix: ".min", extname: ".js" }))
-
-    .pipe(dest(path.build.js))
-
+    .pipe(rename({ suffix: '.min', extname: '.js' }))
+    .pipe(gulp.dest(path.build.js))
     .pipe(browsersync.stream());
 }
 
-async function images() {
-  return src(path.src.images)
+function images() {
+  return gulp.src(path.src.images)
     .pipe(imagemin())
-
-    .pipe(dest(path.build.images));
+    .pipe(gulp.dest(path.build.images));
 }
 
-async function clean() {
-  return gulpclean(path.clean);
+function clean() {
+  return gulp.src(path.clean, { read: false, allowEmpty: true }).pipe(gulpclean());
 }
 
-async function pug() {
-  return src(path.src.pug, { base: "./src/pug/pages/" })
+function pug() {
+  return gulp.src(path.src.pug, { base: './src/pug/pages/' })
     .pipe(gulppug({ pretty: true }))
-
-    .pipe(dest(path.build.html))
+    .pipe(gulp.dest(path.build.html))
     .pipe(browsersync.stream());
 }
 
-async function font() {
-  return src(path.src.font, { base: "src/assets/fonts/" })
-    .pipe(dest(path.build.font))
-
+function font() {
+  return gulp.src(path.src.font, { base: 'src/fonts/' })
+    .pipe(gulp.dest(path.build.font))
     .pipe(browsersync.stream());
 }
 
-async function watchFiles() {
+function watchFiles() {
   gulp.watch([path.watch.pug], pug);
-
   gulp.watch([path.watch.font], font);
-
   gulp.watch([path.watch.css], css);
-
   gulp.watch([path.watch.js], js);
 }
 
 const build = gulp.series(clean, css, font, js, pug, images);
+const watchTask = gulp.series(build, gulp.parallel(watchFiles, browserSync));
 
-const watch = gulp.series(build, watchFiles, browserSync);
-
-exports.css = css;
-
-exports.js = js;
-
-exports.images = images;
-
-exports.clean = clean;
-
-exports.pug = pug;
-
-exports.font = font;
-
-exports.build = build;
-
-exports.watch = watch;
-
-exports.default = watch;
+export { css, js, images, clean, pug, font, build, watchTask as default };
